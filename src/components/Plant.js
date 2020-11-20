c902f18ba6591304e0d63f859ac7f9c98c5d1bd5
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-11-20 15:47:01 +0000

    Style plant card images with sttyled components:

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,4 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledCard = styled.section`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  height: 25rem;
+  width: auto;
+  border: 5px solid black;
+  margin: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  position: absolute;
+  height: 100%;
+  width: auto;
+  z-index: 1;
+  border: 5px solid blue;
+`;
+
+const Img = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  border: 5px solid red;
+`;
 
 export default function Plant({ plant, refreshPlants }) {
   const markPlantPurchased = async () => {
@@ -27,11 +53,13 @@ export default function Plant({ plant, refreshPlants }) {
 
   //returns one component
   return (
-    <div className="list-group-item">
+    <StyledCard>
       {/* the name is hyperlinked */}
       <h4 className="list-group-item-heading">{plant.name}</h4>
       {/* <a href={plant.img_link}></a> */}
-      <img className="img plant" src={plant.img_link} alt={plant.name} />
+      <ImageContainer>
+        <Img className="img plant" src={plant.img_link} alt={plant.name} />
+      </ImageContainer>
       <p>
         {/* if there are tags for the course, display them by creating one span element for each  */}
         Tags:{" "}
@@ -50,6 +78,6 @@ export default function Plant({ plant, refreshPlants }) {
       <button className="btn btn-sm btn-danger ml-2" onClick={deletePlant}>
         Delete
       </button>
-    </div>
+    </StyledCard>
   );
 }
